21c3f6087321d27bd581b3f85ee1b2421f3ca82e
antonfrancisjeejo <antonfrancisjeejo13@gmail.com>
2021-12-10 21:24:53 +0530

    Initial commit

---
+++ client/src/Pages/index.js
@@ -0,0 +1,32 @@
+//Common Pages
+import Welcome from "./Welcome";
+import Home from "./Home";
+import Login from "./Login";
+import Signup from "./Signup";
+import OldStories from "./OldStories";
+
+//Story Creator
+import CreateGame from "./Creator/CreateGame";
+import GameCreator from "./Creator/GameCreator";
+import CreatedGame from "./Creator/CreatedGame";
+
+//Player
+import JoinGame from "./Player/JoinGame";
+import StartGame from "./Player/StartGame";
+import PlayGame from "./Player/PlayGame";
+import GameResult from "./Player/GameResult";
+
+export {
+  Welcome,
+  Home,
+  Login,
+  Signup,
+  OldStories,
+  CreateGame,
+  GameCreator,
+  CreatedGame,
+  JoinGame,
+  StartGame,
+  PlayGame,
+  GameResult,
+};
